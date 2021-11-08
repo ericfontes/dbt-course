aed7a98f14d5bddb852b715a07f69a77f847a370
ericfisico@gmail.com <ericfisico@gmail.com>
2021-11-08 23:46:09 +0000

    Bug-fixed-v2

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,34 +1,30 @@
-with customers as (
-
-    select * from {{ ref('stg_customers') }}
-),
-
-orders as (
-
-    select * from {{ ref('stg_orders') }}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
 ),
 
 payments as (
-
     select * from {{ ref('stg_payments') }}
 ),
 
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
 
-final2 as (
+    from payments
+    group by 1
+),
+
+final as (
 
     select
-       orders.order_id,
-       customers.customer_id,
-       payments.payment_id,
-       payments.amount
-       
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
 
     from orders
-
-    left join customers on (customers.customer_id = orders.customer_id)
-    
-    left join payments on (payments.payment_id = customers.customer_id)
-
+    left join order_payments using (order_id)
 )
 
-select * from final2
+select * from final
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,11 +1,11 @@
-with payments as (
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
-    select
-        id as payment_id,
-        amount
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
 
-    from raw.stripe.payment
-
-)
-
-select * from payments
+from raw.stripe.payment 
